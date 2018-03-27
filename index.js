011f7f5672335c43225f16ba7eeafdde4c65c007
Mathias Jakobsen <mathiasjakobsen@me.com>
2018-03-27 10:24:17 +0200

    Check equal amount of numbers

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const addon = require('./build/Release/optimusPrime.node');
 const optimusPrime = new addon.OptimusPrime();
 
 console.time('Javascript');
-for (var i = 0, len = 100000; i < len; i++) {
+for (var i = 0, len = 10000000; i < len; i++) {
   js.isThatAPrimeNumber(i)
 }
 console.timeEnd('Javascript');
 
 console.time('C++');
-for (var i = 0, len = 10000; i < len; i++) {
+for (var i = 0, len = 10000000; i < len; i++) {
   optimusPrime.isThatAPrimeNumber(i)
 }
 console.timeEnd('C++');
